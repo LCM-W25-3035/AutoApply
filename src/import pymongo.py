0b52429465ae7b92d63037f5d049471997224423
TatiRojasF <tatarojas13@gmail.com>
2025-03-18 11:20:33 -0400

    testing carol github

---
+++ src/import pymongo.py
@@ -0,0 +1,13 @@
+import pymongo 
+
+# MongoDB Connection
+MONGO_URI = "mongodb+srv://DavidRocha:davidoscar@capstone.9ajag.mongodb.net/?retryWrites=true&w=majority&appName=Capstone"
+MONGO_DB_NAME = "jobsDB"
+MONGO_JOBS_COLLECTION = "jobsCollection"
+
+client_mongo = pymongo.MongoClient(MONGO_URI)
+db = client_mongo[MONGO_DB_NAME]
+collection = db[MONGO_JOBS_COLLECTION]
+
+# Load jobs from MongoDB
+jobs_data = list(collection.find({}).limit(5))  # Retrieve everything
